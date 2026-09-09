060a55e8d552e19559db610d8a11d72655937494
Diego Gomes Dos Santos <gomesds.diego@gmail.com>
2026-09-09 19:46:40 +0200

    selfwork 02

--- index.php
+++ index.php
@@ -13,8 +13,6 @@ $string = "Ciao a tutti!";
 //variabile Boolean
 $boolean = true;
 
-
-
 //fare comparire i tipi di dato delle variabili
 var_dump($integer) . "\n";
 var_dump($float) . "\n";
@@ -31,3 +29,17 @@ echo(INTEGER. "\n");
 echo(FLOAT. "\n");
 echo(STRING. "\n");
 echo(BOOLEAN. "\n");
+
+
+/* selfwork 02 */
+
+$text1 = "Marco";
+$text2 = "Hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = 'hai';
+$text7 = 'bevuto';
+$text8 = "tutto.";
+
+echo $text1. " ". $text2. " ". $text3. $text4. " ". $text5. " ". $text6. " ". $text7. " ". $text8;
